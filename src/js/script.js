437d06307f002394911c0c9856213830feb8061e
Mari-Anne <107042762+maeeri@users.noreply.github.com>
2025-09-09 20:16:15 +0300

    added github repositories

--- src/js/script.js
+++ src/js/script.js
@@ -5,22 +5,47 @@ async function fetchJsonData(url) {
 }
 
 async function setData() {
+  // const baseUrl = 'https://maeeri.github.io/'
+  // let studies = (await fetchJsonData(baseUrl +'src/data/studies.json')).studies
   let studies = (await fetchJsonData('src/data/studies.json')).studies
   let studieslist = document.getElementById('studieslist')
+  studies.sort((a, b) => new Date(b.end ? b.end : '9999-12-31') - new Date(a.end ? a.end : '9999-12-31'))
   
   for (let i = 0; i < studies.length; i++) {
     let modalBody = createStudyModalBody(studies[i])
     let title = studies[i].school ? studies[i].school + ": " + studies[i].course : studies[i].course
-    let li = createListElement(title, studies[i].school, modalBody)    
+    let li = createListElement(title, studies[i].school, modalBody)
+    li = addModalTrigger(li, studies[i].school, modalBody)
     studieslist.appendChild(li)
   }
 
+  // let work = (await fetchJsonData(baseUrl +'src/data/work.json')).work
   let work = (await fetchJsonData('src/data/work.json')).work
   let worklist = document.getElementById('worklist')
   for (let i = 0; i < work.length; i++) {
     let modalBody = createWorkModalBody(work[i])
-    let li = createListElement(work[i].company + ': ' + work[i].title, work[i].company, modalBody)
+    let li = createListElement(work[i].company + ': ' + work[i].title)
+    li = addModalTrigger(li, work[i].company, modalBody)
     worklist.appendChild(li)
+  }
+
+  const username = "maeeri"
+  const apiUrl = `https://api.github.com/users/${username}/repos`
+  let repos = (await fetchJsonData(apiUrl))
+  repos
+    .sort((a, b) => b.language - a.language)
+    .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+  
+  let reposlist = document.getElementById('reposlist')
+  for (let i = 0; i < repos.length; i++) {
+    let modalBody = createRepoModalBody(repos[i])
+    let screenName = changeRepoName(repos[i].name)
+    
+    let li = createRepoListElement(repos[i].language ? repos[i].language : 'N/A',
+      new Date(repos[i].created_at).toLocaleDateString(),
+      screenName)
+    li = addModalTrigger(li, screenName, modalBody)
+    reposlist.appendChild(li)
   }
 }
 
@@ -32,17 +57,10 @@ function setModalContent(title, body) {
   myModal.show()
 }
 
-function createListElement(listText, modalTitle, modalBody) {
+function createListElement(listText) {
   let li = document.createElement('li')
   li.innerText = listText
   li.className = 'list-group-item'
-  li.dataBsToggle = 'modal'
-  li.dataBsTarget = '#myModal'
-  li.addEventListener('click', () => {
-    setModalContent(modalTitle, modalBody)
-    let myModal = new bootstrap.Modal(document.getElementById('myModal'))
-    myModal.show()
-  })
   return li
 }
 
@@ -54,11 +72,11 @@ function createStudyModalBody(study) {
   let course = createElement('Course: ', study.course)
   div.appendChild(course)
   if (study.begin) {
-    let begin = createElement('Began: ', study.begin)
+    let begin = createElement('Began: ', new Date(study.begin).toLocaleDateString())
     div.appendChild(begin)
   }
   if (study.end) {
-    let end = createElement('Finished: ', study.end)
+    let end = createElement('Finished: ', new Date(study.end).toLocaleDateString())
     div.appendChild(end)
   }
   if (study.ECTS) {
@@ -72,7 +90,8 @@ function createStudyModalBody(study) {
     a.href = study.website
     a.target = '_blank'
     a.rel = 'noopener noreferrer'
-    a.innerText = study.website
+    a.innerText = study.website.length > 40 ? study.website.substring(8, 40) + '...' : study.website
+    a.title = study.website
     website.appendChild(label)
     website.appendChild(a)
     div.appendChild(website)
@@ -91,17 +110,41 @@ createWorkModalBody = function (work) {
     div.appendChild(description)
   }
   if (work.begin) {
-    let begin = createElement('Began: ', work.begin)
+    let begin = createElement('Began: ', new Date(work.begin).toLocaleDateString())
     div.appendChild(begin)
   }
   if (work.end) {
-    let end = createElement('Ended: ', work.end)
+    let end = createElement('Ended: ', new Date(work.end).toLocaleDateString())
     div.appendChild(end)
   }
   if (work.tech.length > 0) {
     let tech = createElement('Technologies: ', work.tech.join(', '))
     div.appendChild(tech)
   }
+  return div
+}
+
+function createRepoModalBody(repo) {
+  let div = document.createElement('div')
+  let description = createElement('Description: ', repo.description ? repo.description : 'N/A')
+  div.appendChild(description)
+  let language = createElement('Main Language: ', repo.language ? repo.language : 'N/A')
+  div.appendChild(language)
+  let created = createElement('Created at: ', new Date(repo.created_at).toLocaleDateString())
+  div.appendChild(created)
+  let updated = createElement('Last Updated: ', new Date(repo.updated_at).toLocaleDateString())
+  div.appendChild(updated)
+  let linkDiv = document.createElement('div')
+  let linkLabel = createLabel('Link: ')
+  let link = document.createElement('a')
+  link.href = repo.html_url
+  link.target = '_blank'
+  link.rel = 'noopener noreferrer'
+  link.innerText = repo.html_url.length > 40 ? repo.html_url.substring(8, 40) + '...' : repo.html_url
+  link.title = repo.html_url
+  linkDiv.appendChild(linkLabel)
+  linkDiv.appendChild(link)
+  div.appendChild(linkDiv)
   return div
 }
 
@@ -126,6 +169,46 @@ function createElement(labelText, contentText) {
   div.appendChild(content)
   div.className = 'mb-2'
   return div
+}
+
+function changeRepoName(name) {
+  splitname = name.split('-')
+  splitname = splitname.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  return splitname.join(' ')  
+}
+
+function createRepoListElement(text1, text2, text3) {
+  let rowDiv = document.createElement('div')
+  rowDiv.className = 'row'
+  rowDiv.style.width = '100%'
+  let li = document.createElement('li')
+  li.className = 'list-group-item'
+  let div1 = document.createElement('div')
+  div1.innerText = text1
+  div1.style.fontWeight = 'bold'
+  div1.className = 'col-md-2'
+  let div2 = document.createElement('div')
+  div2.className = 'col-md-2'
+  div2.innerText = text2
+  let div3 = document.createElement('div')
+  div3.className = 'col-md-7'
+  div3.innerText = text3
+  rowDiv.appendChild(div1)
+  rowDiv.appendChild(div2)
+  rowDiv.appendChild(div3)
+  li.appendChild(rowDiv)
+  return li
+}
+
+function addModalTrigger(li, modalTitle, modalBody) {
+  li.dataBsToggle = 'modal'
+  li.dataBsTarget = '#myModal'
+  li.addEventListener('click', () => {
+    setModalContent(modalTitle, modalBody)
+    let myModal = new bootstrap.Modal(document.getElementById('myModal'))
+    myModal.show()
+  })
+  return li
 }
 
 setData()
